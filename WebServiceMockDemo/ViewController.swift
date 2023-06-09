5859948be791ef02feed5490d32b545412a60fb0
Jithesh Xavier <jitheshx@qburst.com>
2023-06-09 11:35:30 +0530

    Initial Commit

---
+++ WebServiceMockDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebServiceMockDemo
+//
+//  Created by Jithesh Xavier on 09/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
